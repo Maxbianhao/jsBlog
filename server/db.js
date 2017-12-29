910bcaf02b314d1baa86462a44548048864c81c9
边浩 <binnull@foxmail.com>
2017-12-29 11:37:50 +0800

    解决警告

--- server/db.js
+++ server/db.js
@@ -2,12 +2,12 @@
  * @Author: bianhao 
  * @Date: 2017-12-05 16:57:11 
  * @Last Modified by: bianhao
- * @Last Modified time: 2017-12-06 17:24:16
+ * @Last Modified time: 2017-12-29 10:26:15
  */
 var mongoose = require('mongoose'),
   dbPath = require('./config/db-config').dbPath;
 
-mongoose.connect(dbPath);
+mongoose.connection.openUri(dbPath);
 var db = mongoose.connection;
 
 db.on('error', (err) => {
