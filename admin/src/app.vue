0df9989c851cf8b4e0eff68a881bf1a9460ccc3b
边浩 <binnull@foxmail.com>
2018-02-28 11:13:47 +0800

    简单架构

---
+++ admin/src/app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <lt-layout>
+      <lt-header>
+        <app-header></app-header>
+      </lt-header>
+      <lt-main>
+        <lt-layout :isRow="true">
+          <lt-aside>123</lt-aside>
+          <lt-main>123131</lt-main>
+        </lt-layout>
+      </lt-main>
+    </lt-layout>
+  </div>
+</template>
+
+<script>
+import appHeader from 'src/layout/header/header.vue';
+
+export default {
+  name: 'app',
+  components: { appHeader },
+  data () {
+    return {
+      layoutWidth: '100%',
+      mainLayoutIsRow: true
+    }
+  }
+}
+</script>
+<style lang="less">
+  @import './base/reset.css';
+  .lt-header {
+    height: 80px;
+    background-color: rgb(246, 246, 246);
+  }
+</style>
+
+
---
+++ admin/src/layout/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="admin-header"></div>
+</template>
+<script>
+export default {
+  name: 'appHeader',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
